56a97c3f24e25d400f4d98ce954a957cac79bfe5
yodatime <electriclikethor@gmail.com>
2025-02-13 17:12:29 +0200

    Add project files.

---
+++ Recursion-1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Recursion_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive number: ");
+            Console.WriteLine($"THe sum of it's digits is {Sum(int.Parse(Console.ReadLine()))}");
+        }
+        static int Sum(int n)
+        {
+            int sum = 0;
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                    sum = n % 10 + Sum(n/10);
+                    return sum;
+            }
+            
+        }
+    }
+}
