21184280e64c1ff2ab4e914b090484db4ea9949c
MEGA <MEGA@gmail.com>
2025-02-14 18:30:08 +0200

    3

---
+++ Recursion-3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Recursion_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            Console.WriteLine($"The reversed number is {Reverse(int.Parse(Console.ReadLine()), 0)}");
+        }
+        static int Reverse(int n, int reversed=0)
+        {
+            if (n == 0)
+            {
+                return reversed;
+            }
+            return Reverse(n / 10, reversed * 10 + n % 10);
+        }
+    }
+}
